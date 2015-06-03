e4147bedf59551d3492a617075d7c7855a22f944
Abhishek <abhishek.mech011@gmail.com>
2015-06-03 13:00:58 +0530

    Minor error corrections + Added spiderable package

--- server/social-config.js
+++ server/social-config.js
@@ -4,19 +4,19 @@ ServiceConfiguration.configurations.remove({
     service: 'facebook'
 });
  
-ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: '417530805091822',
-    secret: '6ec7a7472933d5a25e4c7632f2c6c990'
-});
+// ServiceConfiguration.configurations.insert({
+//     service: 'facebook',
+//     appId: '417530805091822',
+//     secret: '6ec7a7472933d5a25e4c7632f2c6c990'
+// });
 
 //This is for Deployed
 
-// ServiceConfiguration.configurations.insert({
-//     service: 'facebook',
-//     appId: '406355799542656',
-//     secret: '83107f6b2f6e4d35bab3663215e4ec77'
-// });
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '406355799542656',
+    secret: '83107f6b2f6e4d35bab3663215e4ec77'
+});
 
 
 
